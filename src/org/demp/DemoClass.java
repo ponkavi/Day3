be7a91ce14ac0ac70615c7fd491e3fd8a163eef2
ponkavi <ponkaviaiyan@gmail.com>
2023-10-31 23:58:37 +0530

    Day3

---
+++ src/org/demp/DemoClass.java
@@ -0,0 +1,104 @@
+package org.demp;
+
+public class DemoClass {
+	public static void main(String[] args) {
+		
+		int a=20,b=30,c=50;	
+		
+		
+		
+		
+		if (a>=b && a>=c) {
+			
+			System.out.println("'A' is greater");
+		}
+		
+		else if (b>=a && b>=c) {
+			
+			System.out.println("'B' is greater");
+		}
+		
+		else {
+			
+			System.out.println("'C' is greater");
+		}	
+			
+			
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
